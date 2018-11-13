6162f09d5abfa41bb31839bdb8e7fe56f2b23d67
menuven <menuven@trapallada>
2018-11-13 09:50:17 +0100

    experimental b

---
+++ src/javaapplication54/Contornos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication54;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author menuven
+ */
+public class Contornos {
+    
+    public void mensaje(){
+        JOptionPane.showMessageDialog(null,"Contornos");
+    }
+    
+    public void pruebaMerge(){
+        JOptionPane.showMessageDialog(null, "Prueba Merge contornos");
+    }
+    
+}
